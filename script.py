dd2822fcb0f0e3d03e5722889df3e5cebf37c8b2
YOUNESS AKARID <149713595+younessakarid@users.noreply.github.com>
2024-03-28 11:36:12 +0000

    FIRST COMMIT

---
+++ script.py
@@ -0,0 +1,7 @@
+N1 = float(input(" Taper le nombre 1 : "))
+N2 = float(input("Taper le nombre 2 : "))
+
+total = float(N1 + N2)
+moyenne = float((N1 + N2)/2) 
+print(" le total est  :" , total)
+print(" le moyenne est : ", moyenne)
