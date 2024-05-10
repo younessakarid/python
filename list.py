5335cd04995256db278bd0b63712ba9e4efecb1c
YOUNESS AKARID <149713595+younessakarid@users.noreply.github.com>
2024-05-10 09:55:41 +0100

    commmit

--- list.py
+++ list.py
@@ -1,3 +1,4 @@
+#declaration de dictionnaire
 
 D = {}
 
@@ -11,13 +12,30 @@ D["age"]= int(input("saiser  ton age:"))
 #afichage de dictionnaire
 
 print(D)
+print("----------------------------")
 
 #afichage par clé
 
 for i in D.items() :
     print(i)
 
+print("----------------------------")    
+
 #afichage  en illustrant la clé est la valeur    
 
 for cle,valeur in D.items() :
     print("la cle est ",cle, "est la valeur est : ",valeur)
+
+print("-----------------------------")    
+
+#afichage  en ullustrant la clé  
+
+for cle in D :
+    print("la cle est : ",cle)
+
+print("-----------------------------")  
+
+#afichage  en ullustrant la valeur
+
+for valeur in D.values() :
+    print("la valeur est : ",valeur)
